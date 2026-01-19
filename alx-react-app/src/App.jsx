531698e2613d823cd10ae51562cbcbc63f88f62d
Lami Adanu <lami.adanu1@gmail.com>
2026-01-20 00:52:00 +0100

    Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,15 @@
 import WelcomeMessage from './components/WelcomeMessage'
 import './App.css'
-
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 function App() {
   return(
-    <WelcomeMessage />
+    <div>
+       <WelcomeMessage />
+       <Header />, <MainContent />, <Footer />
+    </div>
+   
   );
 }
 
